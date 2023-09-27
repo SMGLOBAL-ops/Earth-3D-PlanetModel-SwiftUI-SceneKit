3ec8e6843fe1fda9e7add7562989e506afe5d4f0
Saif Mustafa <68763259+SMGLOBAL-ops@users.noreply.github.com>
2023-09-27 16:57:05 +0100

    readd essential files lost in commit

---
+++ 3DModel-Venus-Rotational-SceneKit/PlanetView.swift
@@ -0,0 +1,63 @@
+import SwiftUI
+import SceneKit
+
+struct PlanetView: View {
+    private let scene = PlanetScene()
+    private let cameraNode = createCameraNode()
+    @State private var initialDragLocation: CGPoint? = nil
+    @State private var currentDragLocation: CGPoint? = nil
+    @State private var horizontalRotationAngle: Angle = .zero
+    @State private var verticalRotationAngle: Angle = .zero
+    @State private var moonRotationAngle: Double = 0
+    
+    // Add a state variable to control the rotation of the second planet
+    @State private var secondPlanetRotationAngle: Double = 0
+
+    var body: some View {
+        ZStack {
+            // SceneView
+            SceneView(scene: scene, pointOfView: cameraNode)
+                .gesture(
+                    DragGesture()
+                        .onChanged { gesture in
+                            if initialDragLocation == nil {
+                                initialDragLocation = gesture.location
+                            }
+                            currentDragLocation = gesture.location
+
+                            // Calculate the rotation angles based on drag movement
+                            let horizontalDelta = Double(currentDragLocation!.x - initialDragLocation!.x)
+                            let verticalDelta = Double(currentDragLocation!.y - initialDragLocation!.y)
+
+                            horizontalRotationAngle = Angle(degrees: horizontalDelta / 5.0)
+                            verticalRotationAngle = Angle(degrees: verticalDelta / 5.0)
+                        }
+                        .onEnded { _ in
+                            initialDragLocation = nil
+                            currentDragLocation = nil
+                        }
+                )
+                .onReceive(AnimationTicker.shared.ticker) { _ in
+                    // Rotate the planet node smoothly
+                    scene.rotatePlanetNode(horizontalRotation: horizontalRotationAngle, verticalRotation: verticalRotationAngle)
+                    
+                    // Rotate the second planet (moon) around the first planet (Venus)
+                    secondPlanetRotationAngle += 0.005 // Adjust the speed of rotation
+                    scene.rotateSecondPlanetNode(angle: secondPlanetRotationAngle)
+                }
+        }
+        Text("Planet Earth with Moon - 3D Model").font(.system(.title3))
+    }
+
+    static func createCameraNode() -> SCNNode {
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        return cameraNode
+    }
+}
+
+struct PlanetView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlanetView()
+    }
+}
---
+++ 3DModel-Venus-Rotational-SceneKit/Planetania.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+@main
+struct Planetania: App {
+    var body: some Scene {
+        WindowGroup {
+            PlanetView()
+        }
+    }
+}
+
+
+
